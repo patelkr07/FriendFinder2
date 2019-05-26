e3af6b009eac4ef1cb3b6684ebe185eb596678f2
Kunal <patelkr07@gmail.com>
2019-05-26 00:31:03 -0500

    adding get routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,13 @@
+
+var tableData = require("../data/friends");
+
+module.exports = function(app) {
+
+    app.get("/api/tables", function(req, res) {
+        res.json(friends);
+    });
+
+    app.post("/api/tables", function(req, res) {
+        res.json(friends);
+    });
+}
